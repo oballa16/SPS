bf73d3203269fca323e440923192f553725d2742
Kristian <ksota16@epoka.edu.al>
2019-04-27 17:32:28 +0200

    Updated Services.blade.php

--- project/resources/views/services.blade.php
+++ project/resources/views/services.blade.php
@@ -1,9 +1,30 @@
 @extends('inc.layout')
-
+@extends('layouts.app')
 @section('title')
     SPS >> Services
 @stop
 
 @section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Services List</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        Welcome to the State Police Services System
+                    </div>
+                    <div class="containsServices">
+                        <button>Make a complaint</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @stop
