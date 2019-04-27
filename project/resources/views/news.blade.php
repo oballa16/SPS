08345a513207c896c58aa6bf5ae41d1a4cbc3afa
Kristian <ksota16@epoka.edu.al>
2019-04-27 23:25:54 +0200

    Added news page, updated web.php

--- project/resources/views/news.blade.php
+++ project/resources/views/news.blade.php
@@ -1,12 +1,101 @@
 @extends('inc.layout')
-
+@extends('layouts.app')
 @section('title')
-SPS >> News
+News
 @stop
-
-
 @section('content')
-
-
-
+    <div class="blog content-area-2">
+        <div class="container">
+            <div class="main-title">
+                <h1>News</h1>
+                <p>Check out some recent news posts.</p>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="blog-1">
+                        <div class="blog-photo">
+                            <img src="http://placehold.it/350x214" alt="blog" class="img-fluid">
+                            <div class="profile-user">
+                                <img src="http://placehold.it/45x45" alt="user">
+                            </div>
+                        </div>
+                        <div class="detail">
+                            <div class="date-box">
+                                <span>27</span>Feb
+                            </div>
+                            <div class="post-meta clearfix">
+                                <ul>
+                                    <li>
+                                        <strong><a href="#">Antony Toms</a></strong>
+                                    </li>
+                                    <li class="float-right mr-0"><a href="#"><i class="fa fa-commenting-o"></i></a>15</li>
+                                    <li class="float-right"><a href="#"><i class="flaticon-time"></i></a>5k</li>
+                                </ul>
+                            </div>
+                            <h3>
+                                <a href="blog-single-sidebar-right.html">Mattis magna neque finibus maximus.</a>
+                            </h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 d-none d-xl-block d-lg-block">
+                    <div class="blog-1">
+                        <div class="blog-photo">
+                            <img src="http://placehold.it/350x214" alt="blog" class="img-fluid">
+                            <div class="profile-user">
+                                <img src="http://placehold.it/45x45" alt="user">
+                            </div>
+                        </div>
+                        <div class="detail">
+                            <div class="date-box">
+                                <span>27</span>Feb
+                            </div>
+                            <div class="post-meta clearfix">
+                                <ul>
+                                    <li>
+                                        <strong><a href="#">Teseira Jeklin</a></strong>
+                                    </li>
+                                    <li class="float-right mr-0"><a href="#"><i class="fa fa-commenting-o"></i></a>15</li>
+                                    <li class="float-right"><a href="#"><i class="flaticon-time"></i></a>5k</li>
+                                </ul>
+                            </div>
+                            <h3>
+                                <a href="blog-single-sidebar-right.html">Attis magna ac neque finibus maximus.</a>
+                            </h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="blog-1">
+                        <div class="blog-photo">
+                            <img src="http://placehold.it/350x214" alt="blog" class="img-fluid">
+                            <div class="profile-user">
+                                <img src="http://placehold.it/45x45" alt="user">
+                            </div>
+                        </div>
+                        <div class="detail">
+                            <div class="date-box">
+                                <span>27</span>Feb
+                            </div>
+                            <div class="post-meta clearfix">
+                                <ul>
+                                    <li>
+                                        <strong><a href="#">Teseira Jeklin</a></strong>
+                                    </li>
+                                    <li class="float-right mr-0"><a href="#"><i class="fa fa-commenting-o"></i></a>15</li>
+                                    <li class="float-right"><a href="#"><i class="flaticon-time"></i></a>5k</li>
+                                </ul>
+                            </div>
+                            <h3>
+                                <a href="blog-single-sidebar-right.html">Sapien eu sem consectetur amet scelerisque.</a>
+                            </h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
